ba01951eb0afcf3125102bbf2d9ecb8f540a3738
David Gardetti <dgardetti@gmail.com>
2015-07-07 15:17:08 -0500

    v1 done

--- global.js
+++ global.js
@@ -1,18 +1,22 @@
-var start = prompt("start value");
-var end = prompt("ending value");
-var countby = prompt("count by");
+var question1 = {question: "How many words are in this sentence?", possibleAnswers: "a. 1 b. 55 c. 345 d. 7 e. 4", answer: "d"};
+var question2 = {question: "What about this sentence?", possibleAnswers: "a. 1 b. 55 c. 345 d. 7 e. 4", answer: "e"};
+var question3 = {question: "And this sentence?", possibleAnswers: "a. 1 b. 55 c. 345 d. 7 e. 3", answer: "e"};
+var question4 = {question: "What's your favorite letter.", possibleAnswers: "a. b. c. d. e.", answer: "b"};
+var question5 = {question: "Is there a correct answer to this question?", possibleAnswers: "a. Yes b. No c. Not Sure d. Maybe", answer: "a"}
 
-start = parseInt(start);
-end = parseInt(end);
-countby = parseInt(countby);
+var questionsArray = [question1, question2, question3, question4, question5];
 
-while (end < start) {
-  end = prompt("enter an ending value greater than " + start);
-  end = parseInt(end);
+var userAnswer;
+
+for (count = 0, score = 0; questionsArray[count]; count++) {
+  console.log(questionsArray[count].question);
+  userAnswer = prompt(questionsArray[count].question + "   " + questionsArray[count].possibleAnswers)
+  console.log(userAnswer)
+  
+  if (userAnswer === questionsArray[count].answer) {
+    score++
+  }
+  
 }
 
-
-while (start < end) {
-    console.log(start);
-    start += countby;
-}
+alert("You scored " + (score/count)*100 + "%")
